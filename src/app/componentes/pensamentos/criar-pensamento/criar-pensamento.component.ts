56b5270393b59c4e58a04ad6f3be6c47f3ea9fe5
Jeice Santos <jeice.santos@gmail.com>
2023-11-02 18:19:18 -0300

    Aplicação usando Angular

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent implements OnInit {
+
+  pensamento: Pensamento = {
+    id:1,
+    conteudo:'aprendendo angular',
+    autoria:'Jeice',
+    modelo: 'modelo1'
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  criarPensamento(){
+    alert("Novo pensamento criado!")
+  }
+  cancelar(){
+    alert("Objeto cancelado")
+  }
+}
